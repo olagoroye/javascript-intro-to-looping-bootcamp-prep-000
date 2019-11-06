ce333aeb97623ad4f5d2c65856dd4ad992fc3e3f
olusola olagoroye <solaolagoroye@gmail.com>
2019-11-06 02:52:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -28,6 +28,16 @@ while (countdown > 0) {
 var i = 0;
  
 function doWhileLoop(num) {
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+} while (incrementVariable() < num);
   
 }
   
